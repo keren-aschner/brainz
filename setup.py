a5a78a008b1fab37a1524a8b59e8a1d7cc31df61
Keren Solodkin <keren.solodkin@gmail.com>
2019-12-09 10:22:32 -0800

    Update .gitignoreAdd setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brain-computer-interface',
+    version='0.1.0',
+    author='Keren Solodkin',
+    description='Project for advanced system design course.',
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=['pytest', 'pytest-cov'],
+)
