59fd965bc485d9e909169c4c4e3efa31b961c961
Keren Solodkin <keren.solodkin@gmail.com>
2020-01-04 07:12:36 -0800

    Add tests for protobuf parser

---
+++ tests/client/mind/test_protobuf_parser.py
@@ -0,0 +1,66 @@
+import json
+import pathlib
+from datetime import datetime, timezone
+
+import pytest
+
+from brain_computer_interface.client.mind.protobuf_parser import ProtobufParser, ParsingError
+
+RESOURCES = pathlib.Path(__file__).absolute().parent.parent.parent / 'resources' / 'client' / 'mind'
+with open(RESOURCES / 'color_image.data', 'r') as f:
+    COLOR_IMAGE = f.read()
+
+with open(RESOURCES / 'depth_image.json', 'r') as f:
+    DEPTH_IMAGE = json.load(f)
+
+
+@pytest.fixture
+def parser():
+    return ProtobufParser()
+
+
+def test_parse_user(parser):
+    f = open(RESOURCES / 'user.proto', 'rb')
+    user = parser.parse_user(f)
+    f.close()
+    assert user['user_id'] == '42'
+    assert user['username'] == 'Dan Gittik'
+    assert user['birthday'] == datetime(1992, 3, 4, 22, tzinfo=timezone.utc).timestamp()
+    assert user['gender'] == 'MALE'
+
+
+def test_parse_snapshot(parser):
+    f = open(RESOURCES / 'snapshot.proto', 'rb')
+    snapshot = parser.parse_snapshot(f)
+    f.close()
+    assert snapshot['timestamp'] == '1575446887339'
+    pose = snapshot['pose']
+    translation = pose['translation']
+    assert translation['x'] == 0.4873843491077423
+    assert translation['y'] == 0.007090016733855009
+    assert translation['z'] == -1.1306129693984985
+    rotation = pose['rotation']
+    assert rotation['x'] == -0.10888676356214629
+    assert rotation['y'] == -0.26755994585035286
+    assert rotation['z'] == -0.021271118915446748
+    assert rotation['w'] == 0.9571326384559261
+    feelings = snapshot['feelings']
+    assert feelings['hunger'] == 0
+    assert feelings['thirst'] == 0
+    assert feelings['exhaustion'] == 0
+    assert feelings['happiness'] == 0
+    color_image = snapshot['color_image']
+    assert color_image['height'] == 1080
+    assert color_image['width'] == 1920
+    assert color_image['data'] == COLOR_IMAGE
+    depth_image = snapshot['depth_image']
+    assert depth_image['height'] == 172
+    assert depth_image['width'] == 224
+    assert depth_image['data'] == DEPTH_IMAGE
+
+
+def test_not_enough_data(parser):
+    f = open(RESOURCES / 'user.proto', 'rb')
+    with pytest.raises(ParsingError):
+        parser.parse_snapshot(f)
+    f.close()
