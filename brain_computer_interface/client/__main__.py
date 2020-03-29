d9171a0a2c80fe0c0d21bc73754ed841b704a188
Keren Solodkin <keren.solodkin@gmail.com>
2020-03-29 16:51:05 +0300

    Improve cli

--- brain_computer_interface/client/__main__.py
+++ brain_computer_interface/client/__main__.py
@@ -10,13 +10,13 @@ def cli():
 
 
 @cli.command('upload-sample')
-@click.option('-h', '--host', default='127.0.0.1', help='The host to upload the snapshots to.')
-@click.option('-p', '--port', default=8000, help='The port of the host to upload the snapshots to.')
+@click.option('-h', '--host', default='127.0.0.1', help='The host to upload the snapshots to. Default is 127.0.0.1.')
+@click.option('-p', '--port', default=8000, help='The port of the host to upload the snapshots to. Default is 8000.')
 @click.argument('sample')
 @click.option('--binary', is_flag=True, help='Use binary sample.')
 def upload(host, port, sample, binary):
     """
-    Upload the given sample to the server on `host`:`port`.
+    Upload SAMPLE to the server on `host`:`port`.
     """
     upload_sample(host, port, sample, not binary)
 
--- brain_computer_interface/server/__main__.py
+++ brain_computer_interface/server/__main__.py
@@ -14,12 +14,12 @@ def cli():
 
 
 @cli.command('run-server')
-@click.option('-h', '--host', default='127.0.0.1', help='The host to listen on.')
-@click.option('-p', '--port', default=8000, help='The port to listen on.')
+@click.option('-h', '--host', default='127.0.0.1', help='The host to listen on. Default is 127.0.0.1.')
+@click.option('-p', '--port', default=8000, help='The port to listen on. Default is 8000.')
 @click.argument('message_queue_url')
 def run(host, port, message_queue_url):
     """
-    Listen on `host`:`port` and pass received messages to the given message_queue.
+    Listen on `host`:`port` and pass received messages to MESSAGE_QUEUE_URL.
     """
     run_server(host, port, publish_to_url(message_queue_url))
 
