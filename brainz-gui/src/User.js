2354d6934c8ee1700b0bcff4f484bec7558f21bd
Keren Solodkin <keren.solodkin@gmail.com>
2020-04-08 01:11:59 +0300

    Add basic chart

--- brainz-gui/src/User.js
+++ brainz-gui/src/User.js
@@ -1,13 +1,57 @@
 import React from 'react';
-import Alert from 'react-bootstrap/Alert'
+import CanvasJSReact from "./canvasjs/canvasjs.react";
+
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 
 class UserCard extends React.Component {
     render() {
+        let user;
+        if (this.props.id === 1) {
+            user = {id: 1, name: "Keren Solodkin", birthday: 856828800.0, gender: 'f'};
+        } else {
+            user = {id: 2, name: "Bar Aschner", birthday: 864259200.0, gender: 'm'};
+        }
+
+        const options = {
+            animationEnabled: true,
+            title: {
+                text: "Monthly Sales - 2017"
+            },
+            axisX: {
+                valueFormatString: "MMM"
+            },
+            axisY: {
+                title: "Sales (in USD)",
+                prefix: "$",
+                includeZero: false
+            },
+            data: [{
+                yValueFormatString: "$#,###",
+                xValueFormatString: "MMMM",
+                type: "spline",
+                dataPoints: [
+                    {x: new Date(2017, 0), y: 25060},
+                    {x: new Date(2017, 1), y: 27980},
+                    {x: new Date(2017, 2), y: 42800},
+                    {x: new Date(2017, 3), y: 32400},
+                    {x: new Date(2017, 4), y: 35260},
+                    {x: new Date(2017, 5), y: 33900},
+                    {x: new Date(2017, 6), y: 40000},
+                    {x: new Date(2017, 7), y: 52500},
+                    {x: new Date(2017, 8), y: 32300},
+                    {x: new Date(2017, 9), y: 42000},
+                    {x: new Date(2017, 10), y: 37160},
+                    {x: new Date(2017, 11), y: 38400}
+                ]
+            }]
+        };
         return (
             <div>
-                <Alert>
-                    User {this.props.id}
-                </Alert>
+                <CanvasJSChart options={options}
+                    /* onRef={ref => this.chart = ref} */
+                />
+                {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
             </div>
         );
     }
--- brainz-gui/src/UserCard.js
+++ brainz-gui/src/UserCard.js
@@ -5,13 +5,22 @@ import Card from 'react-bootstrap/Card';
 
 class UserCard extends React.Component {
     render() {
+        let user;
+        if (this.props.id === 1) {
+            user = {id: 1, name: "Keren Solodkin", birthday: 856828800.0, gender: 'f'};
+        } else {
+            user = {id: 2, name: "Bar Aschner", birthday: 864259200.0, gender: 'm'};
+        }
+
         return (
             <div>
                 <Card style={{width: '18rem'}}>
                     <Card.Body>
-                        <Card.Title>User {this.props.id}</Card.Title>
+                        <Card.Title>{user.name}</Card.Title>
                         <Card.Text>
-                            User data
+                            ID: {user.id}<br/>
+                            Birthday: {new Date(user.birthday * 1000).toDateString()}<br/>
+                            Gender: {user.gender === 'f' ? 'Female' : 'Male'}
                         </Card.Text>
                         <Button onClick={this.props.onClick}>See Snapshots</Button>
                     </Card.Body>
