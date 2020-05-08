e9fc1b044e2efd35f8abee3e2a80075a982be939
Keren Solodkin <keren.solodkin@gmail.com>
2020-05-08 10:38:35 +0300

    Add cli tests for client

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -1,8 +1,11 @@
 import pathlib
 
+import pytest
 from bson.json_util import loads
+from click.testing import CliRunner
 
 from brainz.client import upload_sample
+from brainz.client.__main__ import cli
 from brainz.protocol.fields import *
 
 RESOURCES = pathlib.Path(__file__).absolute().parent.parent / "resources" / "client"
@@ -14,28 +17,42 @@ with open(RESOURCES / "snapshot.bson", "r") as f:
 _USER = {USER_ID: 42, USERNAME: "Dan Gittik", BIRTHDAY: 699746400, GENDER: "MALE"}
 
 _SERVER_HOST = "127.0.0.1"
-_SERVER_PORT = 5000
+_SERVER_PORT = 8000
 _SERVER_ADDRESS = f"{_SERVER_HOST}:{_SERVER_PORT}"
 _CONFIG = [TIMESTAMP, POSE, COLOR_IMAGE, DEPTH_IMAGE, FEELINGS]
 
+SAMPLES = [
+    pytest.param(PROTO_SAMPLE, True, id="protobuf"),
+    pytest.param(BIN_SAMPLE, False, id="binary"),
+]
 
-def test_proto_sample(requests_mock):
+
+@pytest.fixture()
+def requests(requests_mock):
     requests_mock.get(f"http://{_SERVER_ADDRESS}/config", json={"config": _CONFIG})
     requests_mock.post(f"http://{_SERVER_ADDRESS}/snapshot")
+    yield requests_mock
 
-    upload_sample(_SERVER_HOST, _SERVER_PORT, PROTO_SAMPLE, True)
 
-    data = loads(requests_mock.last_request.body)
+@pytest.mark.parametrize("sample,protobuf", SAMPLES)
+def test_sample(requests, sample, protobuf):
+    upload_sample(_SERVER_HOST, _SERVER_PORT, sample, protobuf)
+    data = loads(requests.last_request.body)
+
     assert data[USER] == _USER
     assert data["snapshot"] == _SNAPSHOT
 
 
-def test_bin_sample(requests_mock):
-    requests_mock.get(f"http://{_SERVER_ADDRESS}/config", json={"config": _CONFIG})
-    requests_mock.post(f"http://{_SERVER_ADDRESS}/snapshot")
+@pytest.mark.parametrize("sample,protobuf", SAMPLES)
+def test_cli(requests, sample, protobuf):
+    args = ["upload-sample", str(sample)]
+    if not protobuf:
+        args.append("--binary")
 
-    upload_sample(_SERVER_HOST, _SERVER_PORT, BIN_SAMPLE, False)
-    data = loads(requests_mock.last_request.body)
+    runner = CliRunner()
+    runner.invoke(cli, args)
+    data = loads(requests.last_request.body)
+
     assert data[USER] == _USER
     assert data["snapshot"] == _SNAPSHOT
 
